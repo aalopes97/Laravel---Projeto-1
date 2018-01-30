47035a81245541fdaa602e4615be523d873b4e13
php 20 <php.20@tiaogaviao.cec.ime.usp.br>
2018-01-30 21:49:00 -0200

    Projetinho Laravel

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saudação</title>
+  </head>
+  <body>
+@for ($i=0; $i <10; $i++)
+<h1>{{$i}}</h1>
+@endfor
+  </body>
+</html>
