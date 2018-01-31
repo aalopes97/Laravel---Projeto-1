04706a53ddb8183e7e424d3a0164fe98b643b69a
php 20 <php.20@tiaogaviao.cec.ime.usp.br>
2018-01-31 20:11:05 -0200

    Pagina saudação

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -5,10 +5,9 @@
     <title>Saudação</title>
     <link rel="stylesheet" href="/css/app.css" />
   </head>
-  <body>
-@for ($i=0; $i <10; $i++)
-  <h1 style="text-align:center;">{{$i}}</h1>
-@endfor
+  <body style="background: url('https://hdqwalls.com/download/material-design-blue-and-white-to-1440x900.jpg'); background-position:center; background-size:cover;">
+<div style="width:300px; height:300px; background-color:#efebeb; margin:auto; margin-top:150px; border-radius:6px;">
+</div>
 <script type="text/javascript" src="app.js" > </script>
   </body>
 </html>
