193c5b66eb13945d10fe307dca0decc4063dfbe2
php 20 <php.20@tiaogaviao.cec.ime.usp.br>
2018-01-30 22:22:10 -0200

    Laravelzinho 2

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Saudação</title>
+    <link rel="stylesheet" href="/css/app.css" />
   </head>
   <body>
 @for ($i=0; $i <10; $i++)
-<h1>{{$i}}</h1>
+  <h1 style="text-align:center;">{{$i}}</h1>
 @endfor
+<script type="text/javascript" src="app.js" > </script>
   </body>
 </html>
